27bb43987eee3ef7989725d1c2e4a806c9b41dd5
Vlad <vladskryto@gmail.com>
2020-12-03 02:03:13 +0400

    Remove unused code from Startup

--- Test.SocketChat.Api/Startup.cs
+++ Test.SocketChat.Api/Startup.cs
@@ -1,7 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -14,18 +13,8 @@ namespace Test.SocketChat.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-
             services.AddLogging(builder =>
             {
                 builder.AddConsole()
@@ -37,8 +26,7 @@ namespace Test.SocketChat.Api
             services.AddSingleton<ChatHub>();
             services.AddSingleton<WebsocketMiddleware>();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -46,20 +34,12 @@ namespace Test.SocketChat.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
             app.UseWebSockets(new WebSocketOptions
             {
                 KeepAliveInterval = TimeSpan.FromSeconds(120)
             });
 
             app.UseMiddleware<WebsocketMiddleware>();
-
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
